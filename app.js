304c486b457b1225fd15448533bc50af6c25f0b2
Brandon <mbrandon@labs.neit.edu>
2019-01-17 12:21:30 -0500

    My third commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var timestamp = require('./timeStamp');
 
 http.createServer(function(req,res){
     res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<h1>Welcome to the Homepage!</h1>");
     res.write("The day and time is: " + timestamp.timeStamp() + "<br>");
-    res.end("Welcome to the Homepage!");
+    res.end();
 }).listen(3030);
