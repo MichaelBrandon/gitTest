77f6bd115447049f3197bea56e5bde1d73252b97
Brandon <mbrandon@labs.neit.edu>
2019-01-17 12:53:21 -0500

    My third commit

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-var http = require("http");
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
 var timestamp = require('./timeStamp');
 
 http.createServer(function(req,res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    //file system response
+    /*
+    fs.readFile('index.html', function(err, data){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+    });
+*/
+
     res.write("<h1>Welcome to the Homepage!</h1>");
     res.write("The day and time is: " + timestamp.timeStamp() + "<br>");
-    res.end();
+    res.write("This is the url you requested: " + req.url + "<br>");
+    var q = url.parse(req.url, true).query;
+    var resTxt = q.id + " " + q.user;
+    var qList = url.parse(req.url, true);
+    console.log(qList);
+    console.log(qList.host);//localhost:3030
+    console.log(qList.pathName);//user.html
+    console.log(qList.search);//id and users
+    res.end(resTxt);
+
 }).listen(3030);
