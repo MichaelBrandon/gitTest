b89f9b6a879aa6882d377b704a57a84d3db23fda
Brandon <mbrandon@labs.neit.edu>
2019-01-17 12:17:04 -0500

    My third commit

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-console.log("this is our test for github");
-console.log("this is our test for github");
+var http = require("http");
+var timestamp = require('./timeStamp');
+
+http.createServer(function(req,res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The day and time is: " + timestamp.timeStamp() + "<br>");
+    res.end("Welcome to the Homepage!");
+}).listen(3030);
